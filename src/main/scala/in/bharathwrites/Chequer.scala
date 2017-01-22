55ad1fd0ca3e70faea3a1a1d6bcf840e16327016
bharath12345 <bharath12345@gmail.com>
2017-01-23 00:11:49 +0530

    circular paths problem

--- src/main/scala/in/bharathwrites/Chequer.scala
+++ src/main/scala/in/bharathwrites/Chequer.scala
@@ -4,6 +4,7 @@ import scala.annotation.tailrec
 import scala.util.control.NonFatal
 
 object Chequer extends App {
+
   case class Position(x: Int, y: Int)
   case class Cell(p: Position, mark: Boolean)
 
@@ -19,12 +20,16 @@ object Chequer extends App {
 
     // this a single threaded program; so mutable variables are fine
     private var cells: Map[Position, Cell] =
-      (for {i <- 0 to 9; j <- 0 to 9 }
-        yield {
-          val p = Position(i, j)
-          p -> Cell(p, mark = false)
-        }).toMap
+    (for {i <- 0 to 9; j <- 0 to 9}
+      yield {
+        val p = Position(i, j)
+        p -> Cell(p, mark = false)
+      }).toMap
 
+    val backtracking_threshold = 8 // totally max 8 moves are possible from any position. If num of attempts exceed this for a cell,
+    // then, it is caught in a loop and the traversal has to be backtracked on level higher
+
+    var backtrack_counter: (Position, Int) = _
     var prev_pos: List[Position] = List()
     val move_count = moves.length
     var last_unmarked: Position = _
@@ -32,8 +37,8 @@ object Chequer extends App {
     mark(start_pos, null)
 
     def complete(): Boolean = {
-      val x = cells.forall { case (p, cell) => cell.mark}
-      if(x) println(cells)//cells.foreach(println)
+      val x = cells.forall { case (p, cell) => cell.mark }
+      if (x) println(cells) //cells.foreach(println)
       x && prev_pos.size == 99
     }
 
@@ -50,7 +55,7 @@ object Chequer extends App {
               //println(s"$new_p is already marked")
               None
             case false =>
-              if(curr_pos != null) {
+              if (curr_pos != null) {
                 prev_pos = curr_pos +: prev_pos
               }
 
@@ -92,9 +97,8 @@ object Chequer extends App {
     private def southWestPos(curr_pos: Position) = Position(curr_pos.x - 2, curr_pos.y + 2)
   }
 
-  // similarly move all ways
-  // after every move check if the board is complete
-  // if all ways fail, then backtrack - use a different movement than the one tried last time. if the last move was north, then this time try south
+  // move all ways; after every move check if the board is complete
+  // if all ways fail, then backtrack - use a different movement than the one used last time. if the last move was north, then this time skip north
   // if backtracking leads us back to initial position on the board then there is no way out. end it
   def compute(x: Int, y: Int): (Boolean, List[Position]) = {
 
@@ -102,35 +106,57 @@ object Chequer extends App {
     val board = new Board(p)
     var counter = 0
 
+    // position 'p' is from where traversal on the board is continued
     @tailrec def traverse(p: Position): Boolean = {
 
+      /*
+      moveid: to pick one of the 8 movement directions
+      first return element: if None then game is still ON; if Some(x) then if x = false, solution not possible. true = solution possible
+      second return element: ending position from last move
+       */
       @tailrec def mover(moveid: Int): (Option[Boolean], Option[Position]) = {
-        if(moveid >= board.move_count) {
+        if (moveid >= board.move_count) {
+
           // backtrack
-          //println(s"previous positions = ${board.prev_pos}")
-          board.prev_pos.isEmpty match {
-            case true =>
-              (Option(false), None)
-            case false =>
-              val prev_pos = board.prev_pos.head
-              println(s"backtracked to $prev_pos")
-              board.unmark(p)
-              // unmarked p should be skipped in the next iter of traverse
-              (None, Option(prev_pos))
+          def backtrack(): (Option[Boolean], Option[Position]) = {
+            board.prev_pos.isEmpty match {
+              case true =>
+                (Option(false), None)
+              case false =>
+                val prev_pos = board.prev_pos.head
+                println(s"backtracked to $prev_pos")
+                board.unmark(p)
+                if(board.prev_pos.isEmpty) {
+                  (Option(false), None)
+                } else {
+                  if (board.backtrack_counter != null && board.backtrack_counter._1 == prev_pos) {
+                    println(s"backtrack status = ${board.backtrack_counter}")
+                    board.backtrack_counter = (prev_pos, board.backtrack_counter._2 + 1)
+                    if (board.backtrack_counter._2 > board.backtracking_threshold) {
+                      backtrack()
+                    }
+                  } else {
+                    board.backtrack_counter = (prev_pos, 1)
+                  }
+                  (None, Option(prev_pos)) // prev_pos should be skipped in the next iter of traverse
+                }
+            }
           }
+          backtrack()
+
         } else {
-          if(board.last_unmarked != null && board.pos(moveid)(p) == board.last_unmarked) {
+          if (board.last_unmarked != null && board.pos(moveid)(p) == board.last_unmarked) {
             // skip this move
             mover(moveid + 1)
           } else {
             board.moves(moveid)(p) match {
-              case None => // wrong move
+              case None =>
                 //println(s"incrementing moveid from $moveid")
                 mover(moveid + 1)
 
               case Some(new_p) =>
                 board.last_unmarked = null
-                if(board.complete()) {
+                if (board.complete()) {
                   (Option(true), None)
                 } else {
                   // game on
@@ -143,7 +169,7 @@ object Chequer extends App {
       mover(0) match {
         case (None, Some(pos)) =>
           counter += 1
-          if(counter > 1000) System.exit(0)
+          if (counter > 5000) System.exit(0)
           traverse(pos)
         case (Some(res), None) => res
         case _ => throw new Exception(s"how the hell did it land here")
@@ -155,13 +181,13 @@ object Chequer extends App {
   }
 
   def visit_all() = {
-    val starting_positions: IndexedSeq[Position] = for {i <- 0 to 9; j <- 0 to 9 } yield Position(i, j)
+    val starting_positions: IndexedSeq[Position] = for {i <- 0 to 9; j <- 0 to 9} yield Position(i, j)
     val visits: IndexedSeq[(Boolean, List[Position])] = starting_positions.map { start_pos =>
       compute(start_pos.x, start_pos.y)
     }
     val visit_all = visits.forall(_._1)
     val routes: IndexedSeq[(Position, List[Position])] = starting_positions.zip(visits.map(x => x._2))
-    val answer = if(visit_all) Console.GREEN + "YES!" + Console.RESET else Console.RED + "NO :(" + Console.RESET
+    val answer = if (visit_all) Console.GREEN + "YES!" + Console.RESET else Console.RED + "NO :(" + Console.RESET
     println(Console.BLUE + s"Is it possible for the pawn to visit all tiles on the board following the above rules? "
       + s"Answer: $answer")
     println(Console.BLUE + "Routes\n" + Console.RESET)
